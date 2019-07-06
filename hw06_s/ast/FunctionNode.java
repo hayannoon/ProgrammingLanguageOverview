eb95a59d3da7d7868b5ad386fd2925085259d7a2
hayannoon <46260961+hayannoon@users.noreply.github.com>
2019-07-06 13:40:18 +0900

    HW06

---
+++ hw06_s/ast/FunctionNode.java
@@ -0,0 +1,22 @@
+package parser.ast;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import lexer.TokenType;
+
+
+// 여기를 작성하게 해도 좋을 듯 18.4.10
+public class FunctionNode extends Node{
+	
+	public FunctionType value;
+	
+	@Override
+	public String toString(){
+	
+	}
+
+	public void setValue(TokenType tType) {
+	
+	}
+}
